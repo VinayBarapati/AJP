02be2dfb557ce70bf63c3b916c879e94689d9854
VinayBarapati <barapativinay03@gmail.com>
2024-12-18 15:57:09 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,20 @@
+//Arithmatic operaor
+ public class Arithmatic {
+ public static void main(String args[]) {
+ int value1=10;
+ int value2=20;
+ System.out.println("sum="+(value1+value2));
+ System.out.println("Difference="+(value1-value2));
+ System.out.println("product="+(value1*value2));
+ System.out.println("Quotient="+(value1/value2));
+ System.out.println("Reminder="+(value1%value2));
+ double v1=40;
+ double v2=20;
+ System.out.println("sum="+(v1+v2));
+ System.out.println("Difference="+(v1-v2));
+ System.out.println("product="+(v1*v2));
+ System.out.println("Quotient="+(v1/v2));
+ System.out.println("Reminder="+(v1%v2));
+
+ }
+}
